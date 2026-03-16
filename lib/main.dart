c4a27e789d9c317deb158127366d6968b6521026
yosiaevan <yosiaevan23@gmail.com>
2026-03-16 19:51:05 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To Do List App',
+      debugShowCheckedModeBanner: false,
+      home: TodoPage(),
+    );
+  }
+}
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  DateTime now = DateTime.now();
+
+  String getToday() {
+    String monthDay = "";
+
+    switch (now.month) {
+      case 1:
+        monthDay = "Januari";
+      case 2:
+        monthDay = "Februari";
+      case 3:
+        monthDay = "Maret";
+      case 4:
+        monthDay = "April";
+      case 5:
+        monthDay = "Mei";
+      case 6:
+        monthDay = "Juni";
+      case 7:
+        monthDay = "Juli";
+      case 8:
+        monthDay = "Agustus";
+      case 9:
+        monthDay = "September";
+      case 10:
+        monthDay = "Oktober";
+      case 11:
+        monthDay = "November";
+      case 12:
+        monthDay = "Desember";
+      default:
+        monthDay = "";
+    }
+
+    switch (now.weekday) {
+      case 1:
+        return "Senin, ${now.day} ${monthDay} ${now.year}";
+      case 2:
+        return "Selasa, ${now.day} ${monthDay} ${now.year}";
+      case 3:
+        return "Rabu, ${now.day} ${monthDay} ${now.year}";
+      case 4:
+        return "Kamis, ${now.day} ${monthDay} ${now.year}";
+      case 5:
+        return "Jumat, ${now.day} ${monthDay} ${now.year}";
+      case 6:
+        return "Sabtu, ${now.day} ${monthDay} ${now.year}";
+      case 7:
+        return "Minggu, ${now.day} ${monthDay} ${now.year}";
+      default:
+        return "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text(
+                    getToday(),
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff6a7282),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
+
